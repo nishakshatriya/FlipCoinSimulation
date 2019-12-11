fa8bdcfd0be4d6536fead15bc25f56d17f51f9df
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-11 10:58:58 +0530

    Modified: variable name 'num' to 'Coins'

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -1,21 +1,22 @@
+g13
 #!/bin/bash/ -x
 
 headCount=0
 tailCount=0
 
 declare -A resultDictionary
-declare -A percentageDictionary
+declare -A percentDictionary
 
 echo "WELCOME TO FLIPCOIN SIMULATION"
 read -p "how much time you flip coin " trials
-read -p "how many coins " numberOfCoin
+read -p "how many coins " coins
 
 function flipcoin() 
 {
 	for (( i=1; i<=$trials; i++ ))
 	do
 		string=""
-		for(( j=0; j<$numberOfCoin; j++ ))
+		for(( j=0; j<$coins; j++ ))
 		do
    			outCome=$(( RANDOM % 2 ))
      			if [ $outCome == 1 ]
@@ -39,13 +40,13 @@ echo ${resultDictionary[@]}
 for i in "${!resultDictionary[@]}"
 do
 
-		 percentageDictionary[$i]=$(( (100*${resultDictionary[$i]})/ $trials ))
+		 percentDictionary[$i]=$(( (100*${resultDictionary[$i]})/ $trials ))
 done
 
 #echo $(printf "%s \n " ${percentageDictionary[@]} ${!percentageDictionary[@]} | sort -nr | head -2 )
 
-for i in ${!percentageDictionary[@]}
+for i in ${!percentDictionary[@]}
 do
 	echo $i ${percentDictionary[$i]}
 
-done | sort -k2 -nr | awk 'NR==1{print $1 " " $2}'
+done | sort -k2 -nr | awk 'NR==1{print $1" "$2}'
