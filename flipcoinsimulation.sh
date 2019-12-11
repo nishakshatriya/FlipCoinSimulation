02642c9910ac5ac56ca7864de5004334116765e8
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-11 09:44:55 +0530

    Added: Stored SingletCombination and Percent in Dict

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -1,12 +1,35 @@
 #!/bin/bash/ -x
 
-echo "WELCOME TO FLIPCOIN SIMULATION"
+#LOCAL VARIABLES
+isHead=1
+isTail=0
+HCount=0
+TCount=0
 
-function flipCoin()
-{
+declare -A dicResult
 
-	outCome=$((RANDOM%2))
-	echo $outCome
-}
+echo "WELCOME TO FLIPCOIN SIMULATOR"
+read -p "how many times you want to Flip a coin?" trials
 
-flipCoin
+for((i=0;i<trials;i++))
+do
+
+	result=$((RANDOM%2))
+	if [ $result -eq $isHead ]
+	then
+		HCount=$((Hcount+1))
+		dicResult[heads]=$HCount
+	else
+		TCount=$((TCount+1))
+		dicResult[tail]=$TCount
+	fi
+	echo $result
+done
+
+total=$(( $HCount+$TCount ))
+head=$(( $HCount*100 ))
+headPercent=$(( $head/$total ))
+tail=$(( $TCount*100 ))
+tailPercent=$(( $tail/$total ))
+echo $headPercent
+echo $tailPercent
