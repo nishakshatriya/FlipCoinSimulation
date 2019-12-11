6c823e65f924d2788cc999d9c17c018c8da16825
nishakshatriya <nisha.kshatriya08@gmail.com>
2019-12-11 10:41:58 +0530

    Modified: Sorting Func for WinningCombination

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -27,7 +27,6 @@ function flipcoin()
           			tailCount=$(( $tailCount + 1))
 				fi
 		done
-		echo key : $string
 		resultDictionary["$string"]=$(( ${resultDictionary["$string"]} + 1 ))  
 		done
 }
@@ -43,4 +42,10 @@ do
 		 percentageDictionary[$i]=$(( (100*${resultDictionary[$i]})/ $trials ))
 done
 
-echo $(printf "%s \n " ${percentageDictionary[@]} ${!percentageDictionary[@]} | sort -nr | head -1 )
+#echo $(printf "%s \n " ${percentageDictionary[@]} ${!percentageDictionary[@]} | sort -nr | head -2 )
+
+for i in ${!percentageDictionary[@]}
+do
+	echo $i ${percentDictionary[$i]}
+
+done | sort -k2 -nr | awk 'NR==1{print $1 " " $2}'
